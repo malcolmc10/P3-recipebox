2533bf0b184b8073191272ebd10229d0da63b422
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-06-18 17:44:18 -0400

    [Fixed] Routes

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // import HomeCover from '../Components/HomeCover'
 // import TopPicks from '../Components/TopPicks'
+//
 
 class App extends Component {
   render() {
