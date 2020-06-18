6580100d55491e9d155a19a1b626b2cc8c3d9ab8
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-18 19:40:29 -0400

    formatted code

--- client/src/Components/StepNav.jsx
+++ client/src/Components/StepNav.jsx
@@ -9,37 +9,43 @@ export default class StepNav extends Component {
     }
     render() {
         return (
-      <div>
-        <nav>
-          <div>
-        <a  href="#">
-            <h1>Step 1 <span>{this. state.props.recipe}</span></h1>
-          </a>
-           </div>
-           <div>
-           <a  href="#">
-            <h1>Step 2 <span>{this.state.props.recipe}</span></h1>
-          </a>
-           </div>
-           <div>
-           <a  href="#">
-            <h1>Step 3 <span>{this.state.props.recipe}</span></h1>
-          </a>
-           </div>
-           <div>
-           <a  href="#">
-            <h1>Step 4 <span>{this.state.props.recipe}</span></h1>
-          </a>
-           </div>
-           <div>
-           <a  href="#">
-            <h1>Step 5 <span>{this.state.props.recipe}</span></h1>
-          </a>
-           </div>
-          
-        </nav>
-      </div>
-        )}
+            <div>
+                <nav>
+                    <div>
+                        <a href="#">
+                            <h1>Step 1 <span>{this.state.props.recipe}</span></h1>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <h1>Step 2 <span>{this.state.props.recipe}</span></h1>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <h1>Step 3 <span>{this.state.props.recipe}</span></h1>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <h1>Step 4 <span>{this.state.props.recipe}</span></h1>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="#">
+                            <h1>Step 5 <span>{this.state.props.recipe}</span></h1>
+                        </a>
+                    </div>
+                    <div>
+                        <a href="/">
+                            <h1>Back to Top <span>{"/"}</span></h1>
+                        </a>
+                    </div>
+
+                </nav>
+            </div>
+        )
+    }
 }
 
 
@@ -68,47 +74,4 @@ export default class StepNav extends Component {
 
 
 
-// import React, { Component } from "react";
-// import App from "./src/App";
-// import "index.css";
-
-//  class StepNav extends Component {
-//   render() {
-//     return (
-//       <div>
-//         <nav>
-//           <div>
-//         <a  href="#">
-//             <h1>Step 1 <span>{this.props.recipe}</span></h1>
-//           </a>
-//            </div>
-//            <div>
-//            <a  href="#">
-//             <h1>Step 2 <span>{this.props.recipe}</span></h1>
-//           </a>
-//            </div>
-//            <div>
-//            <a  href="#">
-//             <h1>Step 3 <span>{this.props.recipe}</span></h1>
-//           </a>
-//            </div>
-//            <div>
-//            <a  href="#">
-//             <h1>Step 4 <span>{this.props.recipe}</span></h1>
-//           </a>
-//            </div>
-//            <div>
-//            <a  href="#">
-//             <h1>Step 5 <span>{this.props.recipe}</span></h1>
-//           </a>
-//            </div>
-          
-//         </nav>
-//       </div>
-      
-//     );
-//   }
-// }
- 
-// export default StepNav;
 
