3d62d79c3cfe4877b38b8bfc06de303bb1eab3d9
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-18 19:20:45 -0400

    setting the state

--- client/src/Components/StepNav.jsx
+++ client/src/Components/StepNav.jsx
@@ -1,45 +1,114 @@
+import React, { Component } from 'react'
 
-import React, { Component } from "react";
-import App from "./src/App";
-import "index.css";
-
- 
-class StepNav extends Component {
-  render() {
-    return (
+export default class StepNav extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes: recipe
+        }
+    }
+    render() {
+        return (
       <div>
         <nav>
           <div>
         <a  href="#">
-            <h1>Step 1 <span>{this.props.recipe}</span></h1>
+            <h1>Step 1 <span>{this. state.props.recipe}</span></h1>
           </a>
            </div>
            <div>
            <a  href="#">
-            <h1>Step 2 <span>{this.props.recipe}</span></h1>
+            <h1>Step 2 <span>{this.state.props.recipe}</span></h1>
           </a>
            </div>
            <div>
            <a  href="#">
-            <h1>Step 3 <span>{this.props.recipe}</span></h1>
+            <h1>Step 3 <span>{this.state.props.recipe}</span></h1>
           </a>
            </div>
            <div>
            <a  href="#">
-            <h1>Step 4 <span>{this.props.recipe}</span></h1>
+            <h1>Step 4 <span>{this.state.props.recipe}</span></h1>
           </a>
            </div>
            <div>
            <a  href="#">
-            <h1>Step 5 <span>{this.props.recipe}</span></h1>
+            <h1>Step 5 <span>{this.state.props.recipe}</span></h1>
           </a>
            </div>
           
         </nav>
       </div>
-      
-    );
-  }
+        )}
 }
+
+
+    // thinking for the logic using if else statements to help with the placement when the buttons are pressed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { Component } from "react";
+// import App from "./src/App";
+// import "index.css";
+
+//  class StepNav extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <nav>
+//           <div>
+//         <a  href="#">
+//             <h1>Step 1 <span>{this.props.recipe}</span></h1>
+//           </a>
+//            </div>
+//            <div>
+//            <a  href="#">
+//             <h1>Step 2 <span>{this.props.recipe}</span></h1>
+//           </a>
+//            </div>
+//            <div>
+//            <a  href="#">
+//             <h1>Step 3 <span>{this.props.recipe}</span></h1>
+//           </a>
+//            </div>
+//            <div>
+//            <a  href="#">
+//             <h1>Step 4 <span>{this.props.recipe}</span></h1>
+//           </a>
+//            </div>
+//            <div>
+//            <a  href="#">
+//             <h1>Step 5 <span>{this.props.recipe}</span></h1>
+//           </a>
+//            </div>
+          
+//         </nav>
+//       </div>
+      
+//     );
+//   }
+// }
  
-export default StepNav;
+// export default StepNav;
+
