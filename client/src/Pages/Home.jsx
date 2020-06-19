11b818c21861a46e4c28c94f28b407fe4210cba8
cboulafaa <boulafaacharaf@gmail.com>
2020-06-19 15:57:49 -0400

    added background pattern

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import Header from '../Shared/Header'
 class Home extends Component {
   render() {
     return (
-      <div className="container mx-auto px-4 font-serif">
+      <div className="container bg-white mx-auto px-4 font-serif">
         <Header />
       </div>
     );
