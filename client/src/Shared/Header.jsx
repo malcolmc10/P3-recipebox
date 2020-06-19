892653922bdfdca84e9fccb895f1810cd0ddb1d4
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-19 12:06:14 -0400

    completed header mobile

--- client/src/Shared/Header.jsx
+++ client/src/Shared/Header.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import logo from '../assets/logo_with_text.png'
 import searchIcon from '../assets/zondicons/search.svg'
+import hamburgerIcon from '../assets/zondicons/menu.svg'
 
 
 export default function Header() {
     return (
         <div>
-            <ul className="flex justify-evenly items-center py-4 text-xl text-gray-600">
-                <li><a href="#" className="text-orange-500">My RecipeBox</a></li>
-                <li><a href="#">Recipes</a></li>
-                <li><a href="#">Store</a></li>
-                <li><a href="#"><img src={logo} className="w-20"/></a></li>
-                <li><a href="#">About</a></li>
-                <li><a href="#">Subscribe</a></li>
-                <li><a href="#" className="text-orange-500">Log In</a></li>
+            <ul className="flex sm:justify-evenly items-center py-4 text-xl text-gray-600 justify-between">
+                <li className="sm:hidden"><a href="#" ><img src={hamburgerIcon} className="w-4"/></a></li>
+                <li className="hidden sm:block"><a href="#" className="text-orange-500">My RecipeBox</a></li>
+                <li className="hidden sm:block"><a href="#">Recipes</a></li>
+                <li className="hidden sm:block"><a href="#">Store</a></li>
+                <li><a href="#"><img src={logo} className="w-16"/></a></li>
+                <li className="hidden sm:block"><a href="#">About</a></li>
+                <li className="hidden sm:block"><a href="#">Subscribe</a></li>
+                <li className="hidden sm:block"><a href="#" className="text-orange-500">Log In</a></li>
                 <li><a href="#"><img src={searchIcon} className="w-4"/></a></li>
             </ul>
         </div>
