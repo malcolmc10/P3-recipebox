1605f2754bf3541025f7d81e5a848cbd94442662
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-19 18:02:07 -0400

    added Top 10 Summer foods image

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from '../Shared/Header'
+import HomeCover from '../Components/HomeCover'
 
 class Home extends Component {
   render() {
     return (
       <div className="container bg-white mx-auto px-4 font-serif">
         <Header />
+        <HomeCover />
       </div>
     );
   }
