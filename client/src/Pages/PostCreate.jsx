158cb66908f8cd62cd8a156c995926135d71a995
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-22 14:05:50 -0400

    added css to CreatePost page

--- client/src/Pages/PostCreate.jsx
+++ client/src/Pages/PostCreate.jsx
@@ -7,26 +7,26 @@ export default function PostCreate() {
       <Layout showButton={false}>
 
       <div className="my-5 mx-auto max-w-xl">
-        <h1 className="text-3xl text-bold mb-5">New Post</h1>
-        <form className="">
-          <input class="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Name of Recipe" />
+        <h1 className="text-3xl text-bold text-gray-400 mb-5">Add Your Recipe Here</h1>
+        <form>
+          <input className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Name of Recipe" />
 
-          <input class="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Ingredients Here" />
+          <input className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Ingredients Here" />
           
-          <input class="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
+          <input className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
 
-          <textarea class="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 1 Here"></textarea>
+          <textarea className="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 1 Here"></textarea>
 
-          <input class="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
+          <input className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
 
-          <textarea class="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 2 Here"></textarea>
+          <textarea className="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 2 Here"></textarea>
 
-          <input class="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
+          <input className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
 
-<textarea class="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 3 Here"></textarea>
+<textarea className="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 3 Here"></textarea>
 
 
-          <button class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline" type="button">
+          <button className="bg-orange-500 hover:bg-gray-400 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline" type="button">
             Submit
           </button>
         </form>
