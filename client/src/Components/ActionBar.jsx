dcea14144c561676db5d5d18d0f5976adf3b79f2
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-06-22 17:00:00 -0400

    fixed all the icons on the detail page, refactored for num/dem, other minor changes.

--- client/src/Components/ActionBar.jsx
+++ client/src/Components/ActionBar.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import heart from '../assets/zondicons/heart.svg'
-import share from '../assets/zondicons/share.svg'
+import React, { useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as HeartFilled } from '@fortawesome/free-solid-svg-icons'
+import { faHeart as HeartEmpty } from '@fortawesome/free-regular-svg-icons'
+
 
 export default function ActionBar() {
+
+  const [isClicked, setClick] = useState(false)
+
+
+  const handleHeartClick = () => { 
+    setClick(!isClicked)
+  }
+
   return (
-    <div class="text-2xl text-bold mb-5 text-center flex justify-between">
-      <img src={heart} alt="favorite icon" class="h-10 w-auto" />
-      <img src={share} alt="favorite icon" class="h-10 w-auto" />
+    <div className="text-2xl text-bold mb-5 text-center flex justify-between">
+
+      <button onClick={handleHeartClick}>
+        {isClicked ? <FontAwesomeIcon icon={HeartFilled} className="text-red-500" /> : <FontAwesomeIcon icon={HeartEmpty} />}
+      </button>
+
+
+      <button>
+        <FontAwesomeIcon icon={faExternalLinkAlt} />
+      </button>
+
     </div>
   )
 }
