62a58f7e03f81e0ca64a2f495f3c3e97d72b08c4
Dan Dalgatov <dandalgatov@Dans-MacBook-Air.local>
2020-06-22 03:38:28 -0400

    small changes across app to resolve warnings and to adopt the style

--- seed/seed.js
+++ seed/seed.js
@@ -290,108 +290,94 @@ const main = async () => {
       ],
     },
     {
-      title: "Rice",
-      coverImage: "https://i.imgur.com/3znSL1M.png",
-      servesPeople: 4,
-      cookTime: 1,
-      cookTimeUnit: "Hour",
+      title: "Spanish Rice",
+      coverImage: "https://i.ibb.co/QjJsZrb/Screen-Shot-2020-06-21-at-4-10-05-PM.png",
+      servesPeople: 2,
+      cookTime: 40,
+      cookTimeUnit: "Minutes",
       ingredients: [
         {
-          amount: 1,
-          unit: "tsp",
+          amount: 2,
+          unit: "Tbsp",
           name: "olive oil",
           consistency: "",
-          meta: "",
+          meta: ""
         },
         {
-          amount: 3,
+          amount: 1,
           unit: "clove",
           name: "garlic",
           consistency: "minced",
-          meta: "",
+          meta: ""
         },
         {
           amount: 0.5,
-          unit: "cup",
-          name: "onions",
-          consistency: "diced",
-          meta: "",
+          unit: "",
+          name: "green bell pepper or 1 jalapeno",
+          consistency: "seeded and finely diced",
+          meta: ""
         },
         {
-          amount: 1,
-          unit: "cup",
-          name: "celery",
-          consistency: "diced",
-          meta: "",
+          amount: 2,
+          unit: "",
+          name: "green onions or a handful of cilantro",
+          consistency: "roughly chopped",
+          meta: ""
+        },
+        {
+          amount: 1.5,
+          unit: "cups",
+          name: "Tomato sauce",
+          consistency: "",
+          meta: "(or canned diced tomatoes if you want chunky tomato pieces in the rice)"
+        },
+        {
+          amount: 3,
+          unit: "cups",
+          name: "chicken broth/stock",
+          consistency: "",
+          meta: ""
         },
         {
           amount: 1,
           unit: "tsp",
-          name: "fennel seeds",
-          consistency: "ground",
-          meta: "",
-        },
-        {
-          amount: 3,
-          unit: "",
-          name: "plum tomatoes",
-          consistency: "diced",
-          meta: "",
-        },
-        {
-          amount: 4,
-          unit: "cup",
-          name: "chicken broth",
+          name: "salt",
           consistency: "",
-          meta: "",
+          meta: "(or according to taste if the chicken broth is salty)"
         },
         {
-          amount: 2,
-          unit: "",
-          name: "chicken breasts",
-          consistency: "diced",
-          meta: "",
-        },
-        {
-          amount: 2,
-          unit: "",
-          name: "potatoes",
-          consistency: "",
-          meta: "",
-        },
-        {
-          amount: 1,
-          unit: "pinch",
-          name: "garlic powder",
-          consistency: "",
-          meta: "",
-        },
-        {
-          amount: null,
-          unit: "",
+          amount: 1/3,
+          unit: "tsp",
           name: "black pepper",
-          consistency: "",
-          meta: "to taste",
+          consistency: "ground",
+          meta: ""
         },
+        {
+          amount: .5,
+          unit: "tsp",
+          name: "oregano",
+          consistency: "dried",
+          meta: ""
+        }
       ],
       preparation: [
         {
           step: 1,
           description:
-            "Sauté the olive oil, garlic, and onion over medium heat in a large nonstick stockpot.",
-          stepImage: "https://i.imgur.com/mVgiec0.png",
+            "Heat 2 tbsp of olive oil in a deep pan, on medium-high heat. Add the minced garlic and sautee for a minute or two, until it’s soft. Toss in the diced green pepper/ jalapeno and sautee it together with the garlic. Before your garlic starts burning, add the rice and stir it well so that more or less each grain is coated in oil. Sautee the rice for a couple minutes stirring to make sure the rice doesn’t stick to the bottom of the pan.",
+          stepImage: "https://i.ibb.co/YZkgWtG/Screen-Shot-2020-06-21-at-4-10-21-PM.png"
         },
         {
           step: 2,
           description:
-            "Add the celery and fennel and sauté briefly, then add the tomatoes.",
-          stepImage: "https://i.imgur.com/J6UU6Yw.png",
+            "Then add 1/2 cup of tomato sauce or diced tomatoes (according to your preference) and stir it all to coat the rice. Pour in the chicken stock/broth and stir picking up all the flavor from the bottom of the pan. Season the rice with salt, pepper and oregano. Stir gently and let the broth come to a boil on high heat.",
+          stepImage: "https://i.ibb.co/g94B88d/Screen-Shot-2020-06-21-at-4-11-43-PM.png"
         },
         {
           step: 3,
           description:
-            "Add the broth, chicken, potatoes, and seasonings. Cook at a simmer over medium heat until the chicken is done and the potatoes are tender, about 20-25 minutes.",
-          stepImage: "https://i.imgur.com/7QIhXPs.png",
+            "As soon as the liquid starts bubbling, put a lid on and turn the heat down to a low simmer and let the rice cook for about 20 mins, until most of the liquid is absorbed and the rice is tender. Add the chopped green onion or cilantro and very gently stir it in, or simple fluff the rice up with some forks. Garnish with a little green onion on top.",
+          stepImage: "https://i.ibb.co/fF2SbBL/Screen-Shot-2020-06-21-at-4-12-43-PM.png"
         },
       ],
       filters: {
@@ -399,7 +385,7 @@ const main = async () => {
         seasonal: false,
         min30: false,
         glutenFree: false,
-        vegetarian: false,
+        vegetarian: true,
         breakfast: false,
         lunch: true,
         dinner: false,
@@ -410,17 +396,17 @@ const main = async () => {
         japanese: false,
         french: false,
         italian: false,
-        mexican: false,
+        mexican: true,
         american: false,
         canadian: false,
         korean: false,
-        polish: false,
+        polish: false
       },
       comments: [
         {
           commentAuthor: "Dan Dalgatov",
           commentDetails: "This dish is perfect for a hungry developer",
-          commentTime: "1592459790537",
+          commentTime: "1592459790537"
         },
       ],
     },
