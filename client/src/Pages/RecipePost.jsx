6a54ffa5789eb1507accc5a9241e6be471cef042
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-22 15:54:55 -0400

    added Header and Footer to component

--- client/src/Pages/RecipePost.jsx
+++ client/src/Pages/RecipePost.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Header from '../Shared/Header'
+import Footer from '../Shared/Footer'
 
 
-export default function PostCreate() {
+export default function RecipePost() {
   return (
     <>
-      
+      <Header />
 
       <div className="my-5 mx-auto max-w-xl">
         <h1 className="text-3xl text-bold text-gray-400 mb-5">Add Your Recipe Here</h1>
@@ -23,7 +25,7 @@ export default function PostCreate() {
 
           <input className="border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Image URL Here" />
 
-<textarea className="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 3 Here"></textarea>
+        <textarea className="h-56 border border-gray-400 rounded-lg py-2 px-4 mb-4 block w-full appearance-none" type="text" placeholder="Add Step 3 Here"></textarea>
 
 
           <button className="bg-orange-500 hover:bg-gray-400 text-white py-2 px-5 rounded focus:outline-none focus:shadow-outline" type="button">
@@ -32,7 +34,7 @@ export default function PostCreate() {
         </form>
 
         </div>
-        
+       <Footer /> 
     </>
   )
 }
