273a4df2600748994843cd80f714d40ecbe715f6
Colin Malcolm <colinmalcolm8@gmail.com>
2020-06-23 17:53:41 -0400

    completed rendering of top dishes and images

--- client/src/Components/TopPicks.jsx
+++ client/src/Components/TopPicks.jsx
@@ -1,31 +1,25 @@
-import React, {useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
 import Recipe from './Recipe'
-import {getRecipes} from '../Services/recipes'
+import { getRecipes } from '../Services/recipes'
 
 export default function TopPicks() {
-    useEffect(() => {
-      (async () => {
-        const recipes = await getRecipes()
-        console.log(recipes)
-      })()
-      
-    }, [])
+  const [recipes, setRecipes] = useState(null)
+  useEffect(() => {
+    (async () => {
+      let recipesData = await getRecipes()
+      setRecipes(recipesData)
+    })()
 
+  }, [])
 
+console.log(recipes)
   return (
     <div className="py-12">
       <h2 className="text-3xl sm:text-5xl text-center text-orange-500">Top Picks</h2>
       <div className="flex flex-wrap justify-evenly">
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
-        <Recipe />
+
+        {recipes && recipes.map((recipe) => <Recipe recipe={recipe}/>)}
+        
       </div>
     </div>
   )
