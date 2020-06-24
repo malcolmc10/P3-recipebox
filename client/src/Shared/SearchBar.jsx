ebd073b5771fb0b126191277148abf1b3724cad1
cboulafaa <boulafaacharaf@gmail.com>
2020-06-24 12:31:31 -0400

    initialized results page

--- client/src/Shared/SearchBar.jsx
+++ client/src/Shared/SearchBar.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import searchIcon from '../assets/zondicons/search.svg'
+import { Link } from 'react-router-dom'
 
 export default function SearchBar() {
   return (
     <div className="w-3/5 relative">
       <input type="text" className="bg-gray-200 border border-gray-300 rounded-full h-8 outline-none px-3 w-full" />
-      <button className="absolute outline-none" style={{ top: '8px', right: '15px' }}>
-        <img className="w-4" src={searchIcon} alt="search icon"/>
-      </button>
+      <Link className="absolute outline-none" style={{ top: '8px', right: '15px' }}>
+        <img className="w-4" src={searchIcon} alt="search icon" />
+      </Link>
     </div>
   )
 }
